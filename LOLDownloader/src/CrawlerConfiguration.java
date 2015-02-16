10a4054101f1bb8fbe249c19ef8b14ef2b23d5d4
Kent Caspersen <kentcasps@msn.com>
2015-02-16 11:57:51 +0100

    API key stored in separate file

--- LOLDownloader/src/CrawlerConfiguration.java
+++ LOLDownloader/src/CrawlerConfiguration.java
@@ -1,10 +1,15 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Scanner;
 
 
 public class CrawlerConfiguration {
 
 	//Settings
-	String api_key = "d3f768b0-5701-4167-b643-d54fc76dd90d";
+	String api_key;
+	String api_key_file = "api-key.txt";
 	String region = "euw";
 	int request_delay_ms = 1500;
 	int first_match_id = 1971892117; //Match played February 15th 2015. Crawl will start here and go backwards.
@@ -13,29 +18,39 @@ public class CrawlerConfiguration {
 	int offset;
 	int matches_to_crawl;
 	String path;
-	
-		public CrawlerConfiguration(){
+
+	public CrawlerConfiguration(){
+		
 		Scanner in = new Scanner(System.in);
 		String str_input;
-		
+		//Get output path
 		System.out.print("\nOutput path (default C://lol-crawl): ");
 		path = in.nextLine();
 		path = path.equals("") ? "C://lol-crawl" : path;
 		if (!path.endsWith("/"))
 			path += "/";
 		
+		LoadApiKey();
+		if (api_key == null || api_key == ""){
+			System.out.print("Something went wrong. Mare sure the api-key is stored correctly as raw text in: " + path + api_key_file);
+			System.exit(0);
+		}
+		
+	    //Get matches per file
 		System.out.print("Matches per file (default 100): ");
 		str_input = in.nextLine();
 		matches_per_file = 100;
 		if (tryParseInt(str_input))
 			matches_per_file = Integer.parseInt(str_input);
 		
+		//Get crawl offset
 		System.out.print("\nCrawl offset (default 0): ");
 		str_input = in.nextLine();
 		offset = 0;
 		if (tryParseInt(str_input))
 			offset = Integer.parseInt(str_input);
 		
+		//Get matches to crawl
 		System.out.print("\nMatches to crawl (default 1000): ");
 		str_input = in.nextLine();
 		matches_to_crawl = 1000;
@@ -43,6 +58,19 @@ public class CrawlerConfiguration {
 			matches_to_crawl = Integer.parseInt(str_input);
 		
 		in.close();
+	}
+	
+	void LoadApiKey(){
+		//load API key file
+	    try {
+	    	BufferedReader br = new BufferedReader(new FileReader(path + api_key_file));
+	        String line = br.readLine();
+        	if (line != null)
+        		api_key = line;
+        	br.close();
+	    }
+	    catch (IOException e) {
+		}
 	}
 	
 	boolean tryParseInt(String value)  
