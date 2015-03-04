4ca458991cec0f7bf6497c42a849b6dc18586d23
Mathias Andersen <wireless_dk@h15.aau1x.klient.slv.site.aau.dk>
2015-03-04 10:51:02 +0100

    hdfs

--- python/program.py
+++ python/program.py
@@ -1,8 +1,8 @@
 from linearregression.hero_team_win import hero_team_win
 from pyspark import SparkContext
 import glob
-
-sc = SparkContext("local", "Linear Regression Basic", 
+					#hdfs://node1:9000/user/hduser/*.txt // local
+sc = SparkContext("hdfs://node1:9000/user/hduser/*.txt", "Linear Regression Basic", 
 					pyFiles=['/Users/wireless_dk/D802f15/python/matchfilter/matchfilter.py', 
 							 '/Users/wireless_dk/D802f15/python/utility/utility.py'])
 data = sc.textFile(','.join(glob.glob('../LoLMiner/LoLMiner/src/mined-data/region-euw-start-1971603524-size-1000.txt')))  #"data.txt")
