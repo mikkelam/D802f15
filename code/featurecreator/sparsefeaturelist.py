213a10f7b4ca0f816fae2105f11e8167716c10c7
Kent Caspersen <kentcasps@msn.com>
2015-03-17 13:13:44 +0100

    featurecreator up and running

---
+++ code/featurecreator/sparsefeaturelist.py
@@ -0,0 +1,4 @@
+class SparseFeatureList:
+    def __init__(self, sparse_feature_list, feature_count):
+        self.sparse_feature_list = sparse_feature_list
+        self.feature_count = feature_count
