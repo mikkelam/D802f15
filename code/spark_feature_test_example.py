244d8db2d6d3689fb442e9eb70ef01774c1697f6
Datdrol <andreas.b.eriksen@gmail.com>
2015-04-01 14:26:17 +0200

    new test in spark_feature

--- code/spark_feature_test_example.py
+++ code/spark_feature_test_example.py
@@ -11,21 +11,19 @@ team_crossteam = [FeatureType.BLUE_TEAM_SINGLES, FeatureType.RED_TEAM_SINGLES, F
 
 mf = MatchFilter({"matchMode": ["CLASSIC"],
 	                          "matchType": ["MATCHED_GAME"],
-	                          "queueType": ["RANKED_SOLO_5x5", "RANKED_PREMADE_5x5", "NORMAL_5x5_BLIND", "NORMAL_5x5_DRAFT", "GROUP_FINDER_5x5"],
-	                          "participants->*->highestAchievedSeasonTier": ["MASTER", "CHALLENGER", "DIAMOND", "PLATINUM", "GOLD", "SILVER", "BRONZE", "UNRANKED"] ,
+	                          "queueType": ["RANKED_SOLO_5x5", "RANKED_PREMADE_5x5", "NORMAL_5x5_BLIND", "NORMAL_5x5_DRAFT"],
 	                          "participants->*->timeline->xpPerMinDeltas->*": ["!0"]})
-	
 
 	
 outputpath ="/Users/andreaseriksen/Desktop/Project F15/code/data/eval/"
-inputpath = "/Users/andreaseriksen/Desktop/Project F15/code/data/valid/"
+inputpath = "/Users/andreaseriksen/Downloads/test/"
 
 sc = SparkContext("local", "Simple App")
 
 sft = SparkFeatureTest(outputpath, inputpath, mf)
 
-#sft.run("teams", team_features, sc)
+sft.run("spark_single_team", team_features, sc)
 #sft.run("combos", combo_features, sc)
 #sft.run("teams_combos", team_combo, sc)
-sft.run("team_combo_cross", team_combo_cross, sc)
-sft.run("team_crossteam", team_crossteam, sc)
+#sft.run("team_combo_cross", team_combo_cross, sc)
+#sft.run("team_crossteam", team_crossteam, sc)
