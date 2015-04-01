7a8ea6e3520da893d3ee062a69a8004aa50a9916
Datdrol <andreas.b.eriksen@gmail.com>
2015-04-01 10:34:18 +0200

    Added support for multi labelpoints

--- code/featurecreator/sparkexample.py
+++ code/featurecreator/sparkexample.py
@@ -20,10 +20,9 @@ def matchfilter(line):
 		return False
 
 # Load and parse the data
-def parsePoint(single_match, testfeatures, redlabel=False):
+def parsePoint(single_match, testfeatures, feature_creator, redlabel=False):
 	#Generates the features of the game
 	match = json.loads(single_match)
-	feature_creator = FeatureCreator()
 	feature_creator.set_match(match)
 	
 	for feature in testfeatures:
@@ -47,26 +46,42 @@ data = sc.textFile(','.join(glob.glob('/Users/andreaseriksen/Desktop/Project F15
 
 traning_data, eval_data = data.filter(lambda line: matchfilter(line)).randomSplit([0.9, 0.1], 1)
 
-blueparsedData = traning_data.map(lambda line: parsePoint(line, [FeatureType.BLUE_TEAM_SINGLES]))
-redparsedData = traning_data.map(lambda line: parsePoint(line, [FeatureType.RED_TEAM_SINGLES], True))
+fc = FeatureCreator()
+blueparsedData = traning_data.map(lambda line: parsePoint(line, [FeatureType.BLUE_TEAM_SINGLES], fc))
+redparsedData = traning_data.map(lambda line: parsePoint(line, [FeatureType.RED_TEAM_SINGLES], fc, True))
+blueeval = eval_data.map(lambda line: parsePoint(line, [FeatureType.BLUE_TEAM_SINGLES], fc))
+redeval = eval_data.map(lambda line: parsePoint(line, [FeatureType.RED_TEAM_SINGLES], fc, True))
+doubleparsedData = blueparsedData.union(redparsedData)
+doubleeval = blueeval.union(redeval)
 
-doubleparsedData = traning_data.map(lambda line: parsePoint(line, [FeatureType.BLUE_TEAM_SINGLES, FeatureType.RED_TEAM_SINGLES]))  #blueparsedData.union(redparsedData)
 #redparsedData
 # Build the model
-#parsedData = traning_data.map(lambda line: parsePoint(line, [FeatureType.BLUE_TEAM_SINGLES]))
-model = LogisticRegressionWithSGD.train(doubleparsedData)
+fc = FeatureCreator()
+parsedData = traning_data.map(lambda line: parsePoint(line, [FeatureType.BLUE_TEAM_SINGLES,FeatureType.RED_TEAM_SINGLES], fc))
+singleeval = eval_data.map(lambda line: parsePoint(line, [FeatureType.BLUE_TEAM_SINGLES,FeatureType.RED_TEAM_SINGLES], fc))
 
-
-double_count = doubleparsedData.count()
-#print float(eval_parsedData.count())
-prediction = doubleparsedData.map(lambda p: (p.label,  model.predict(p.features)))
-error = prediction.filter(lambda (v, p): v != p).count()
-	
 outputpath ="/Users/andreaseriksen/Desktop/Project F15/code/data/eval/"
 
-filedouble = open(outputpath + "representation_test_single" + ".txt",'w')
-filedouble.write(" count: " + str(double_count) + "\n")
-filedouble.write(" prediction error: " + str(error)+"\n")
+
+doublemodel = LogisticRegressionWithSGD.train(doubleparsedData)
+singlemodel = LogisticRegressionWithSGD.train(parsedData)
+
+
+def __eval__(parsedData, model, name):
+	count = parsedData.count()
+	#print float(eval_parsedData.count())
+	prediction = parsedData.map(lambda p: (p.label,  model.predict(p.features)))
+	error = prediction.filter(lambda (v, p): v != p).count()
+	
+	outputpath ="/Users/andreaseriksen/Desktop/Project F15/code/data/eval/"
+
+	file = open(outputpath + name + ".txt",'w')
+	file.write("count: " + str(count) + "\n")
+	file.write("prediction error: " + str(error)+"\n")
+	file.close()
+	
+__eval__(singleeval, singlemodel, "singlepoint_test_eval")
+__eval__(doubleeval, doublemodel, "doublepoint_test_eval")
 	#print("Training Error = " + str(trainErr))
 
 	#print "test", test_count
