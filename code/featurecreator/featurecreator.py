5ec0744b11347bd4e12f79db56d43c152bf91c7b
Kent Caspersen <kentcasps@msn.com>
2015-04-20 09:55:39 +0200

    fixed bug in init team pairs, that did not init features but only add the features

--- code/featurecreator/featurecreator.py
+++ code/featurecreator/featurecreator.py
@@ -40,9 +40,7 @@ class FeatureCreator:
             FeatureType.BEST_RANK: lambda: self.__best_rank(),
             FeatureType.PATCH_VERSION: lambda: self.__patch_version(),
             FeatureType.SPELL_CHAMPION_COMBO: lambda: self.__spell_champion_combo(), 
-            FeatureType.LANE_CHAMPION_COMBO: lambda: self.__lane_champion_combo(), 
-
-
+            FeatureType.LANE_CHAMPION_COMBO: lambda: self.__lane_champion_combo(),
         }
         self.feature_init_functions = {
             FeatureType.BLUE_TEAM_SINGLES: lambda: self.__init_single_team_champions("BLUE"),
@@ -136,7 +134,6 @@ class FeatureCreator:
         m = self.champion_count
         for c1 in champion_list_blue:
             for c2 in champion_list_red:
-
                 c1_name = FeatureCreator.champion_names[c1]
                 c2_name = FeatureCreator.champion_names[c2]
                 feature_name = c1_name + "-BLUE-VS-" + c2_name + "-RED"
@@ -145,12 +142,11 @@ class FeatureCreator:
     def __init_single_team_pairs(self, team_name):
         # 2-permutations of champions on a single team
         m = FeatureCreator.champion_count
-        for _,c1 in self.champion_names.items():
-            for _,c2 in self.champion_names.items():
-                c1_name = FeatureCreator.champion_names[c1]
-                c2_name = FeatureCreator.champion_names[c2]
-                feature_name = c1_name + "&" + c2_name + "-" + team_name
-                self.__add_feature(feature_name)
+        for c1_id, c1_name in self.champion_names.items():
+            for c2_id, c2_name in self.champion_names.items():
+                if c1_id < c2_id:
+                    feature_name = c1_name + "&" + c2_name + "-" + team_name
+                    self.__init_feature(feature_name)
 
     def __make_single_team_pairs(self, champion_list, team_name):
         # 2-permutations of champions on a single team
@@ -208,7 +204,7 @@ class FeatureCreator:
 
     def __init_spell_champion_combo(self):
         #team?
-        for _,c in self.champion_names.items():
+        for _,c in FeatureCreator.champion_names.items():
             for id1,spell1 in self.summoner_spells.items():
                 for id2,spell2 in self.summoner_spells.items():
                     if id1 > id2:
@@ -232,7 +228,7 @@ class FeatureCreator:
             self.__add_feature(feature_name)
 
     def __init_lane_champion_combo(self):
-        for _,c in self.champion_names.items():
+        for _,c in FeatureCreator.champion_names.items():
             for id1,spell1 in enumerate(self.lane):
                 if id1 > id2:
                     continue
